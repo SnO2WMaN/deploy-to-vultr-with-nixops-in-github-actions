c84a3029e9d142cae7c9cb31f5ebb827606cace8
SnO2WMaN <me@sno2wman.net>
2022-07-09 07:50:27 +0900

    add nixops-plugged

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     devshell.url = "github:numtide/devshell";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixops-plugged.url = "github:lukebfox/nixops-plugged";
 
+    flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
@@ -13,24 +14,33 @@
   outputs = {
     self,
     nixpkgs,
+    nixops-plugged,
     flake-utils,
     devshell,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            devshell.overlay
-          ];
-        };
-      in {
-        devShells.default = pkgs.devshell.mkShell {
-          imports = [
-            (pkgs.devshell.importTOML ./devshell.toml)
-          ];
-        };
-      }
-    );
+    (
+      flake-utils.lib.eachDefaultSystem (
+        system: let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              devshell.overlay
+              (final: prev: nixops-plugged.packages."${system}")
+            ];
+          };
+        in {
+          devShells.default = pkgs.devshell.mkShell {
+            imports = [
+              (pkgs.devshell.importTOML ./devshell.toml)
+            ];
+          };
+        }
+      )
+    )
+    // {
+      nixopsConfigurations = {
+        default = import ./nixops {inherit nixpkgs;};
+      };
+    };
 }
