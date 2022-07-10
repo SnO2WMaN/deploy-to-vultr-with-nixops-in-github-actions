942446d290bf3721927af2982ac78436af842031
SnO2WMaN <me@sno2wman.net>
2022-07-11 02:16:21 +0900

    load from secrets.json

--- nixops/webserver/default.nix
+++ nixops/webserver/default.nix
@@ -3,14 +3,16 @@
   resources,
   pkgs,
   ...
-}: {
+}: let
+  secrets = builtins.fromJSON (builtins.readFile ../secrets.json);
+in {
   imports = [
     ../profiles/vultr.nix
   ];
 
   deployment = {
-    targetHost = "";
-    targetPort = 22;
+    targetHost = secrets.hosts.webserver.host;
+    targetPort = secrets.hosts.webserver.port;
   };
 
   networking.hostName = "webserver";
